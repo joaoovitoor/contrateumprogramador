2b862a27bd4c88ef37a7fdb4423495f165e2d270
joaoovitoor <joaoovitoor@hotmail.com>
2023-03-03 15:42:42 -0300

    remove desenvolvedore

--- components/Main.js
+++ components/Main.js
@@ -3,7 +3,7 @@ export default function Main() {
     <section className="text-gray-600 body-font">
       <div className="max-w-5xl pt-52 pb-24 mx-auto">
         <h1 className="text-80 text-center font-4 lh-6 ld-04 font-bold text-white mb-6">
-          Contratação de Desenvolvedore para Empresas de Médio Porte
+          Contratação de Desenvolvedor para Empresas de Médio Porte
         </h1>
         <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700 text-center">
           Soluções Personalizadas de Desenvolvimento de Software para sua
