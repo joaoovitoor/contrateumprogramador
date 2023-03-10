a696fe5ec9f8f48806d7549bd52e41884df7620b
joaoovitoor <joaoovitoor@hotmail.com>
2023-03-10 13:07:10 -0300

    remove tracking id

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 const GA_TRACKING_ID = "G-4FGQJD8B0B"; // Substitua pelo seu código de acompanhamento
-const ADS_TRACKING_ID = "AW-11109257615";
 
 export default class MyDocument extends Document {
   render() {
@@ -9,41 +8,23 @@ export default class MyDocument extends Document {
       <>
         <Html lang="pt-br">
           <Head>
-            <Head>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${GA_TRACKING_ID}', {
-          page_path: window.location.pathname,
-        });
-      `,
-                }}
-              />
-              {/* Global Site Tag (gtag.js) - Google Ads */}
-
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${ADS_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${ADS_TRACKING_ID}');
-      `,
-                }}
-              />
-            </Head>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
           </Head>
           <body>
             <Main />
