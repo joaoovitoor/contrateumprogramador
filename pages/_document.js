cb4d59d7ea2d1987689d00252c48975c6972f7b7
joaoovitoor <joaoovitoor@hotmail.com>
2023-03-10 13:21:19 -0300

    Multiple tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 const GA_TRACKING_ID = "G-4FGQJD8B0B"; // Substitua pelo seu código de acompanhamento
+const ADS_TAG = "AW-11109257615"; // Substitua pelo seu código de acompanhamento
 
 export default class MyDocument extends Document {
   render() {
@@ -20,6 +21,24 @@ export default class MyDocument extends Document {
                   function gtag(){dataLayer.push(arguments);}
                   gtag('js', new Date());
                   gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+            />
+
+            {/* Global Site Tag (gtag.js) - Google ads */}
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${ADS_TAG}`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${ADS_TAG}', {
                     page_path: window.location.pathname,
                   });
                 `,
